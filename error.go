48e65e34fba8d62a7bcad19c80cc6ca3cdbcb61a
Marcel Blijleven <marcelblijleven@gmail.com>
2020-04-06 19:57:00 +0200

    style: apply gofmt

--- error.go
+++ error.go
@@ -4,6 +4,6 @@ import "errors"
 
 // ErrInvalidCredentials is returned when a user does not provide a username and/or password for the Goflight Client
 var ErrInvalidCredentials = errors.New("incorrect Client credentials received")
+
 // ErrUnauthorizedAccess is returned when the provided username and password don't have access to the resource
 var ErrUnauthorizedAccess = errors.New("you don't have permission to access this resource (403)")
-
