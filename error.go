17834a62a1ce3bd54c1e0c03c3adf21728361fcb
Marcel Blijleven <marcelblijleven@gmail.com>
2020-04-07 14:33:30 +0200

    feat: add functionality for /api/flights/all
    Functionality for /api/flights/all is added through client.Flights.GetFlightsInTime

--- error.go
+++ error.go
@@ -2,8 +2,14 @@ package goflight
 
 import "errors"
 
-// ErrInvalidCredentials is returned when a user does not provide a username and/or password for the Goflight Client
-var ErrInvalidCredentials = errors.New("incorrect Client credentials received")
+// ErrInvalidCredentials is returned when a user does not provide a username and/or password for the Goflight client
+var ErrInvalidCredentials = errors.New("incorrect client credentials received")
 
 // ErrUnauthorizedAccess is returned when the provided username and password don't have access to the resource
 var ErrUnauthorizedAccess = errors.New("you don't have permission to access this resource (403)")
+
+// ErrEndBeforeBegin is returned when the provided end time is before the begin time
+var ErrEndBeforeBegin = errors.New("the provided end time is before the begin time")
+
+// ErrTimeRangeTooBig is returned when the time range parameters are too far apart
+var ErrTimeRangeTooBig = errors.New("the provided time range is more than the maximum of 2 hours")
